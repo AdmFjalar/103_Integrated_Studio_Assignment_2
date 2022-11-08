75e8dc4006d797dfde8e4d2d676a26ecfae2faa0
AdmFjalar <oliver@gronkrans.com>
2022-11-09 10:43:17 +1300

    Re-added Base-Member header file content

--- School_Information_System/Base_Member.h
+++ School_Information_System/Base_Member.h
@@ -1,5 +1,23 @@
 #pragma once
+#include<string>
+
+using std::string;
+
 class Base_Member
 {
+	// stopping things from being accessed from anywhere within the program
+private:
+	string name, password;
+	int age = 0, id = 0, userAccessLevel = 0;
+
+	// allowing to be accessed from anywhere within the program
+public:
+
+	Base_Member();
+	Base_Member(int _age, int _id, string _name, string _password, int _userAccessLevel);
+
+	void createUserAccount(string _name, string _password, int _userAccessLevel, int _id);
+
+	void userLogin(string _name, string _password);
 };
 
