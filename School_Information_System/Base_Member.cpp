c823a9fe3f01230cf966ff5165507ca3c4b45f35
AdmFjalar <oliver@gronkrans.com>
2022-11-09 10:45:47 +1300

    Re-added Base-Member cpp file content

--- School_Information_System/Base_Member.cpp
+++ School_Information_System/Base_Member.cpp
@@ -1,1 +1,16 @@
 #include "Base_Member.h"
+#include <string>
+
+using std::string;
+
+Base_Member::Base_Member() {
+
+}
+
+Base_Member::Base_Member(int _age, int _id, string _name, string _password, int _userAccessLevel) {
+	age = _age;
+	id = _id;
+	name = _name;
+	password = _password;
+	userAccessLevel = _userAccessLevel;
+}
